f73e809842db8e8ddaf04d88efbc210343cb25a3
Madhav Rajan <madh.rajan91@gmail.com>
2019-12-02 19:43:51 -0500

    ci formatted date and amount with moment and numeral

--- src/components/ExpenseListItemComponent.js
+++ src/components/ExpenseListItemComponent.js
@@ -2,14 +2,18 @@
 import React from 'react';
 
 
-
-
-
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+
 
 const ExpenseListItemComponent = (props) => (
     <div>
-        <p><Link to={`/edit/${props.expense.id}`}>{props.expense.description}</Link> {props.expense.amount} {props.expense.createdAt}</p>
+        <p><Link to={`/edit/${props.expense.id}`}>{props.expense.description}</Link> </p>
+        <p>
+            {numeral(props.expense.amount/100).format('$0,0.00')} 
+            {moment(props.expense.createdAt).format('MMMM Do, YYYY')}
+        </p>
         
         </div>
 );
